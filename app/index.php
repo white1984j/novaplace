bcbd2cbb1ed6a5cb5d29976c10f81015b353e040
white1984j <white1984j@gmail.com>
2018-04-18 15:47:59 +0300

    add header

---
+++ app/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+	<meta charset="utf-8">
+	<!-- <base href="/"> -->
+
+	<title>OptimizedHTML 4</title>
+	<meta name="description" content="">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Template Basic Images Start -->
+	<meta property="og:image" content="path/to/image.jpg">
+	<link rel="icon" href="img/favicon/favicon.ico">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png">
+	<!-- Template Basic Images End -->
+	
+	<!-- Custom Browsers Color Start -->
+	<meta name="theme-color" content="#000">
+	<!-- Custom Browsers Color End -->
+
+	<link rel="stylesheet" href="css/main.min.css">
+
+</head>
+
+<body>
+
+	<div class="wrapper">
+		<header class="b-header">
+			<div class="container no-gutters">
+				<div class="row no-gutters">
+					<div class="col content-sm">
+						<div class="b-header__logo">
+							<a href="/">
+								<img src="img/logo.png" alt="logo">
+							</a>
+						</div>
+					</div>
+					<div class="col content-bg">
+						<div class="b-header-top-line row justify-content-between align-items-center">
+							<div class="col-auto">
+								<ul class="b-nav">
+									<li class="b-nav__li">
+										<a href="#" class="b-nav__link">О компании</a>
+									</li>
+									<li class="b-nav__li">
+										<a href="#" class="b-nav__link">Новости</a>
+									</li>
+									<li class="b-nav__li">
+										<a href="#" class="b-nav__link">Отзывы</a>
+									</li>
+									<li class="b-nav__li">
+										<a href="#" class="b-nav__link">Контакты</a>
+									</li>
+								</ul>
+							</div>
+							<div class="col-auto">
+								<a href="tel:89032928968" class="link-tel b-header-link">8-903-292-89-68</a>
+								<a href="#" class="btn btn--red">Оставить заявку</a>
+							</div>
+						</div>
+						<div class="row align-items-end">
+							<div class="col-auto">
+								<a href="#" class="btn-filter">
+									<i class="icon icon-filter-white"></i>
+									<span>Фильтр</span>
+								</a>
+							</div>
+							<div class="col">
+								<form class="b-search">
+									<input type="text" class="b-search__input" placeholder="Поиск по сайту">
+									<button type="submit" class="b-search__submit icons-hover"  >
+										<i class="icon icon-loupe-blue icons-hover--visible"></i>
+										<i class="icon icon-loupe-red"></i>
+									</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<main>
+			<div class="container no-gutters">
+				<div class="row no-gutters">
+					<div class="col content-sm">
+						1
+					</div>
+					<div class="col content-bg">
+						2
+					</div>
+				</div>
+			</div>
+		</main>
+		<footer>
+			footer
+		</footer>
+	</div>
+
+	<script src="js/scripts.min.js"></script>
+
+</body>
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var syntax        = 'sass'; // Syntax: sass or scss;
+
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browsersync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		rsync         = require('gulp-rsync'),
+		spritesmith    = require('gulp.spritesmith');
+
+gulp.task('browser-sync', function() {
+	browsersync({
+		proxy: "novaplace/startgulpproject/app",
+		notify: false,
+		// open: false,
+		// tunnel: true,
+		// tunnel: "projectname", //Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+gulp.task('rsync', function() {
+	return gulp.src('app/**')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Includes files to deploy
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('sprite', function () {
+	var spriteData = gulp.src('app/img/sprite/icons/*.png').pipe(spritesmith({
+		imgName: 'sprite.png',
+		cssName: 'sprite.css'
+	}));
+	return spriteData.pipe(gulp.dest('app/img/sprite'));
+});
+
+gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.php', browsersync.reload)
+});
+
+gulp.task('default', ['watch']);
