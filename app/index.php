df61a05ae3df1ed695a3793472f3ccfd7c696f4e
white1984j <white1984j@gmail.com>
2018-04-18 17:08:46 +0300

    add sidebar

--- app/index.php
+++ app/index.php
@@ -64,7 +64,7 @@
 						</div>
 						<div class="row align-items-end">
 							<div class="col-auto">
-								<a href="#" class="btn-filter">
+								<a href="#" class="btn btn--bg btn--filter">
 									<i class="icon icon-filter-white"></i>
 									<span>Фильтр</span>
 								</a>
@@ -87,7 +87,161 @@
 			<div class="container no-gutters">
 				<div class="row no-gutters">
 					<div class="col content-sm">
-						1
+						<div class="b-sidebar">
+							<div class="b-sidebar__head">
+								<a href="#" class="btn btn--bg btn--red w-100">Каталог недвижимости</a>
+							</div>
+							<div class="b-sidebar-path">
+								<div class="b-sidebar-path__header">
+									<a href="#">Аренда офиса</a>
+									<button class="b-sidebar-path__btn">
+										<i class="icon icon-arrow-white"></i>
+									</button>
+								</div>
+								<div class="b-sidebar-path__body">
+									<ul class="b-sidebar-path__list">
+										<li><a href="#">#1</a></li>
+										<li><a href="#">#2</a></li>
+										<li><a href="#">#3</a></li>
+										<li><a href="#">#4</a></li>
+										<li><a href="#">#5</a></li>
+										<li><a href="#">#6</a></li>
+										<li><a href="#">#7</a></li>
+										<li><a href="#">#8</a></li>
+										<li><a href="#">#9</a></li>
+										<li><a href="#">#10</a></li>
+										<li><a href="#">#11</a></li>
+										<li><a href="#">#12</a></li>
+										<li><a href="#">#13</a></li>
+										<li><a href="#">#14</a></li>
+										<li><a href="#">#15</a></li>
+										<li><a href="#">#16</a></li>
+										<li><a href="#">#17</a></li>
+										<li><a href="#">#18</a></li>
+										<li><a href="#">#19</a></li>
+										<li><a href="#">#20</a></li>
+										<li><a href="#">#21</a></li>
+										<li><a href="#">#22</a></li>
+										<li><a href="#">#23</a></li>
+										<li><a href="#">#24</a></li>
+										<li><a href="#">#25</a></li>
+										<li><a href="#">#26</a></li>
+										<li><a href="#">#27</a></li>
+										<li><a href="#">#28</a></li>
+										<li><a href="#">#29</a></li>
+										<li><a href="#">#30</a></li>
+										<li><a href="#">#31</a></li>
+										<li><a href="#">#32</a></li>
+										<li><a href="#">#33</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="b-sidebar-path active">
+								<div class="b-sidebar-path__header">
+									<a href="#">Аренда офиса с юридическим офисом</a>
+									<button class="b-sidebar-path__btn">
+										<i class="icon icon-arrow-white"></i>
+									</button>
+								</div>
+								<div class="b-sidebar-path__body" style="display: block">
+									<ul class="b-sidebar-path__list">
+										<li><a href="#">#1</a></li>
+										<li><a href="#">#2</a></li>
+										<li><a href="#">#3</a></li>
+										<li><a href="#">#4</a></li>
+										<li><a href="#">#5</a></li>
+										<li><a href="#">#6</a></li>
+										<li><a href="#">#7</a></li>
+										<li><a href="#">#8</a></li>
+										<li><a href="#">#9</a></li>
+										<li><a href="#">#10</a></li>
+										<li><a href="#">#11</a></li>
+										<li><a href="#">#12</a></li>
+										<li><a href="#">#13</a></li>
+										<li><a href="#">#14</a></li>
+										<li><a href="#">#15</a></li>
+										<li><a href="#">#16</a></li>
+										<li><a href="#">#17</a></li>
+										<li><a href="#">#18</a></li>
+										<li><a href="#">#19</a></li>
+										<li><a href="#">#20</a></li>
+										<li><a href="#">#21</a></li>
+										<li><a href="#">#22</a></li>
+										<li><a href="#">#23</a></li>
+										<li><a href="#">#24</a></li>
+										<li><a href="#">#25</a></li>
+										<li><a href="#">#26</a></li>
+										<li><a href="#">#27</a></li>
+										<li><a href="#">#28</a></li>
+										<li><a href="#">#29</a></li>
+										<li><a href="#">#30</a></li>
+										<li><a href="#">#31</a></li>
+										<li><a href="#">#32</a></li>
+										<li><a href="#">#33</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="b-sidebar-path">
+								<div class="b-sidebar-path__header">
+									<a href="#">Здания и особняки</a>
+								</div>
+							</div>
+							<div class="b-sidebar-path">
+								<div class="b-sidebar-path__header">
+									<a href="#">Аренда склада</a>
+								</div>
+							</div>
+							<div class="b-sidebar-path">
+								<div class="b-sidebar-path__header">
+									<a href="#">Помещение свободного назначения</a>
+								</div>
+							</div>
+							<div class="b-sidebar-path">
+								<div class="b-sidebar-path__header">
+									<a href="#">Продажа</a>
+									<button class="b-sidebar-path__btn">
+										<i class="icon icon-arrow-white"></i>
+									</button>
+								</div>
+								<div class="b-sidebar-path__body">
+									<ul class="b-sidebar-path__list">
+										<li><a href="#">#1</a></li>
+										<li><a href="#">#2</a></li>
+										<li><a href="#">#3</a></li>
+										<li><a href="#">#4</a></li>
+										<li><a href="#">#5</a></li>
+										<li><a href="#">#6</a></li>
+										<li><a href="#">#7</a></li>
+										<li><a href="#">#8</a></li>
+										<li><a href="#">#9</a></li>
+										<li><a href="#">#10</a></li>
+										<li><a href="#">#11</a></li>
+										<li><a href="#">#12</a></li>
+										<li><a href="#">#13</a></li>
+										<li><a href="#">#14</a></li>
+										<li><a href="#">#15</a></li>
+										<li><a href="#">#16</a></li>
+										<li><a href="#">#17</a></li>
+										<li><a href="#">#18</a></li>
+										<li><a href="#">#19</a></li>
+										<li><a href="#">#20</a></li>
+										<li><a href="#">#21</a></li>
+										<li><a href="#">#22</a></li>
+										<li><a href="#">#23</a></li>
+										<li><a href="#">#24</a></li>
+										<li><a href="#">#25</a></li>
+										<li><a href="#">#26</a></li>
+										<li><a href="#">#27</a></li>
+										<li><a href="#">#28</a></li>
+										<li><a href="#">#29</a></li>
+										<li><a href="#">#30</a></li>
+										<li><a href="#">#31</a></li>
+										<li><a href="#">#32</a></li>
+										<li><a href="#">#33</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
 					</div>
 					<div class="col content-bg">
 						2
--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,7 @@
 $(function() {
 
-	// Custom JS
+	$('.b-sidebar-path__btn').on("click", function() {
+		$(this).closest('.b-sidebar-path').toggleClass('active').find('.b-sidebar-path__body').slideToggle()
+	});
 
 });
