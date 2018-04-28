25378f8994381478a3e1ce2cbdbe9c3e755b1603
white1984j <white1984j@gmail.com>
2018-04-28 14:19:30 +0300

    add taxation page

---
+++ app/taxation.php
@@ -0,0 +1,141 @@
+<? include('header.php') ?>
+
+			<div class="container no-gutters">
+				<div class="row no-gutters">
+					<div class="col content-sm d-none d-lg-flex">
+						<? include('sidebar.php'); ?>
+					</div>
+					<div class="col content-bg b-content">
+						<div class="pos-r">	
+							<div class="b-filter">
+								<? include('filter.php') ?>
+							</div>
+						</div>
+						<main class="">
+							<div class="b-content__path">
+
+									<h2>Аренда офиса с юридическим офисом</h2>
+									<div class="row">
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+										<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+											<a href="#" class="b-taxation">
+												<span class="b-taxation__icon">
+													<i class="icon icon-doc"></i>
+												</span>
+												<span class="b-taxation__name">Налоговая №1</span>
+											</a>
+										</div>
+									</div>
+
+							</div>
+
+						</main>
+						
+						<? include('footer.php'); ?>
+
+					</div>
+				</div>
+			</div>
+		
+
+	</div>
+
+	<script src="js/scripts.min.js"></script>
+
+</body>
+</html>
