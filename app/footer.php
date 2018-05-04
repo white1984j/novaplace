12a422ac49a3a3331764d35dab431f442631754f
white1984j <white1984j@gmail.com>
2018-05-04 14:20:10 +0300

    finish adaptive modal filter

--- app/footer.php
+++ app/footer.php
@@ -81,123 +81,189 @@
 				<span class="b-tabs__item">Карта</span>
 			</div>
 		</div>
-		<div class="modal-filter__body">
+
 			<div class="b-tab-content">
 				<div class="b-tab-content__item">
-					<i class="circle-preloader"></i>
-					<? include('metro.php'); ?>
+					<div class="modal-filter__body">
+						<i class="circle-preloader"></i>
+						<? include('metro.php'); ?>
+					</div>
+					<div class="modal-filter__footer row">
+						<div class="col-4">
+							<b>Выбрано:</b>
+							<input type="text" class=" b-input" style="margin-left: 15px; width: 100px;" readonly>
+						</div>
+						<div class="col-4">
+							<button class="btn btn--red">Применить</button>
+						</div>
+						<div class="col-4">
+							<input type="reset" class="btn btn--hov-blue" value="Очистить метро">
+						</div>
+					</div>
 				</div>
 				<div class="b-tab-content__item">
-					<i class="circle-preloader"></i>
-					<ul class="b-filter-district-list">
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">ВАО</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">ЗАО</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">Новомосковский</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">САО</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">СВАО</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">СЗАО</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">ЦАО</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">ЮАО</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">ЮВАО</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-						<li>
-							<div class="b-filter-district-path">
-								<label class="b-filter-label-checkbox">
-									<input type="checkbox">
-									<span class="b-filter-label-checkbox__txt">ЮЗАО</span>
-								</label>
-								<span class="b-filter-district-path__count">153 объекта</span>
-							</div>
-						</li>
-					</ul>
+					<div class="modal-filter__body">
+						<i class="circle-preloader"></i>
+						<ul class="b-filter-district-list">
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">ВАО</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">ЗАО</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">Новомосковский</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">САО</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">СВАО</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">СЗАО</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">ЦАО</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">ЮАО</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">ЮВАО</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+							<li>
+								<div class="b-filter-district-path">
+									<label class="b-filter-label-checkbox">
+										<input type="checkbox">
+										<span class="b-filter-label-checkbox__txt">ЮЗАО</span>
+									</label>
+									<span class="b-filter-district-path__count">153 объекта</span>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<div class="modal-filter__footer row">
+						<div class="col-4">
+							<b>Выбрано:</b>
+							<input type="text" class=" b-input" style="margin-left: 15px; width: 100px;" readonly>
+						</div>
+						<div class="col-4">
+							<button class="btn btn--blue">Применить</button>
+						</div>
+						<div class="col-4">
+							<input type="reset" class="btn btn--hov-blue" value="Очистить округ">
+						</div>
+					</div>
 				</div>
 				<div class="b-tab-content__item">
-					<i class="circle-preloader"></i>
-					<? include('filter-area.php'); ?>
+					<div class="modal-filter__body">
+						<i class="circle-preloader"></i>
+						<? include('filter-area.php'); ?>
+					</div>
+					<div class="modal-filter__footer row">
+						<div class="col-4">
+							<b>Выбрано:</b>
+							<input type="text" class=" b-input" style="margin-left: 15px; width: 100px;" readonly>
+						</div>
+						<div class="col-4">
+							<button class="btn btn--red">Применить</button>
+						</div>
+						<div class="col-4">
+							<input type="reset" class="btn btn--hov-blue" value="Очистить районы">
+						</div>
+					</div>
 				</div>
 				<div class="b-tab-content__item">
-					<i class="circle-preloader"></i>
-					<select class="js-select-styler" multiple="" data-selected="[]" name="street[]" tabindex="-1" aria-hidden="true"></select>
+					<div class="modal-filter__body">
+						<i class="circle-preloader"></i>
+						<select class="js-select-styler" multiple="" data-selected="[]" name="street[]" tabindex="-1" aria-hidden="true"></select>
+					</div>
+					<div class="modal-filter__footer row">
+						<div class="col-4">
+							<b>Выбрано:</b>
+							<input type="text" class=" b-input" style="margin-left: 15px; width: 100px;" readonly>
+						</div>
+						<div class="col-4">
+							<button class="btn btn--red">Применить</button>
+						</div>
+						<div class="col-4">
+							<input type="reset" class="btn btn--hov-blue" value="Очистить улицы">
+						</div>
+					</div>
 				</div>
 				<div class="b-tab-content__item">
-					<i class="circle-preloader"></i>
-					<? include('filter-map.php'); ?>
+					<div class="modal-filter__body">
+						<i class="circle-preloader"></i>
+						<? include('filter-map.php'); ?>
+					</div>
+					<div class="modal-filter__footer row">
+						<div class="col-4">
+							<b>Выбрано:</b>
+							<input type="text" class=" b-input" style="margin-left: 15px; width: 100px;" readonly>
+						</div>
+						<div class="col-4">
+							<button class="btn btn--red">Применить</button>
+						</div>
+						<div class="col-4">
+							<input type="reset" class="btn btn--hov-blue" value="Очистить карту">
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="modal-filter__footer">
-			footer
-		</div>
 	</div>
 </div>
